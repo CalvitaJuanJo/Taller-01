3e19581261d531c10371c6e9c1f051dc4c2d2b97
CalvitaJuanJo <juanjosecalva427@gmail.com>
2024-10-17 15:25:46 -0500

    segundo commit

--- proyecto01/src/proyecto01/Proyecto01.java
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -16,6 +16,7 @@ public class Proyecto01 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Ejemplo basico de Java Versionado a traves de Git Manejado desde GitHub");
+        System.out.println("creado por CalvitaJuanjo");
     }
     
 }
